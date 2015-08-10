a344873fb28be22bb5e4b848e2d189a4ed59cd2f
dongweij <jdw19930725@gmail.com>
2015-08-10 18:44:38 +0800

    Finished 17.1

--- src/Problem1.java
+++ src/Problem1.java
@@ -2,7 +2,17 @@
  * Created by dongweij on 2015/8/10.
  */
 public class Problem1 {
+    public static void swapNumIinplace(int a, int b) {
+        // Example for a = 9, b = 4
+        a = a - b; // a = 9 - 4 = 5
+        b = a + b; // b = 5 + 4 = 9
+        a = b - a; // a = 9 - 5
+
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+    }
+
     public static void main(String[] args) {
-        System.out.println("abc");
+        swapNumIinplace(100, 10);
     }
 }
