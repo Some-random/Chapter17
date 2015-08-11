4dfd714d49a373582eabca7acca1b9a643c5e9c4
dongweij <jdw19930725@gmail.com>
2015-08-11 09:24:06 +0800

    Problem statement is way to unclear... skip it...

---
+++ src/Problem10.java
@@ -0,0 +1,9 @@
+/**
+ * Created by dongweij on 2015/8/11.
+ */
+public class Problem10 {
+    //This problem statement is way too unclear... skip it...
+    public static void main(String args[]) {
+
+    }
+}
